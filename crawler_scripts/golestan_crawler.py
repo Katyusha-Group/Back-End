6552b34fe09ac64f4bc43d2e3e50c94cc836e267
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-03-28 14:16:39 +0330

    Fixed sleeping

--- crawler_scripts/golestan_crawler.py
+++ crawler_scripts/golestan_crawler.py
@@ -60,6 +60,7 @@ class GolestanCrawler(SeleniumCrawler):
                 self.driver.close()
 
     def get_captcha(self):
+        time.sleep(1)
         soup = self.get_soup()
         png_url = soup.find('img', {'id': 'imgCaptcha'})['src']
         img_path = self.image_handler.download(png_url)
@@ -69,6 +70,7 @@ class GolestanCrawler(SeleniumCrawler):
         return captcha_text
 
     def verify_login(self) -> bool:
+        time.sleep(1)
         logged_in = self.driver.find_elements(by=By.ID, value='_mt_usr')
         return len(logged_in) > 0
 
@@ -80,7 +82,6 @@ class GolestanCrawler(SeleniumCrawler):
             return self.login_admin()
 
     def login_user(self):
-        time.sleep(2)
         self.switch_to_inner_frames(self.get_form_body(1))
         self.fill_input("F80351", self.username)
         self.fill_input("F80401", self.password)
@@ -88,7 +89,6 @@ class GolestanCrawler(SeleniumCrawler):
         return is_logged_in
 
     def login_admin(self) -> bool:
-        time.sleep(2)
         self.switch_to_inner_frames(self.get_form_body(1))
         is_logged_in = self.pass_captcha()
         return is_logged_in
@@ -98,7 +98,6 @@ class GolestanCrawler(SeleniumCrawler):
         next_captcha = 'a'
         is_logged_in = False
         while not is_logged_in and i < 5:
-            time.sleep(2)
             curr_captcha = next_captcha
             next_captcha = self.get_captcha()
             self.fill_input("F51701", curr_captcha)
@@ -106,7 +105,6 @@ class GolestanCrawler(SeleniumCrawler):
                 self.driver.find_element(by=By.XPATH, value='//*[@id="dsetting"]/label[5]').click()
             else:
                 self.click_on_button("btnLog")
-            time.sleep(2)
             is_logged_in = self.verify_login()
             i += 1
         return is_logged_in
@@ -115,7 +113,7 @@ class GolestanCrawler(SeleniumCrawler):
         self.switch_to_inner_frames(self.get_form_body(2))
         self.fill_input("F20851", "102")
         self.click_on_button("OK")
-        time.sleep(2)
+        time.sleep(0.5)
 
     def go_to_this_term_courses(self, available=True):
         if self.user_login:
@@ -125,9 +123,9 @@ class GolestanCrawler(SeleniumCrawler):
         self.fill_input('GF10956_0', int(available))
         self.switch_to_inner_frames(self.get_commander(self.get_number()))
         self.click_on_button("IM16_ViewRep")
-        time.sleep(4)
 
     def extract_courses(self):
+        time.sleep(0.5)
         self.driver.switch_to.default_content()
         soup = self.get_soup()
         courses = []
@@ -147,8 +145,6 @@ class GolestanCrawler(SeleniumCrawler):
             self.remove_disable_attr('ExToEx')
         self.click_on_button('ExToEx')
         self.switch_to_child_window(window_title=self.driver.window_handles[1])
-        time.sleep(1)
         self.extract_courses()
         self.close_all_windows(parent_window_handle=self.driver.window_handles[0])
         self.switch_to_parent_window(parent_window_handle=self.driver.window_handles[0])
-        time.sleep(2)
