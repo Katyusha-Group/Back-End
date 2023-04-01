c11dbfab4562eca6ccd3c63bab3c369667c7d315
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-04-01 18:43:31 +0330

    made changes of renaming project

--- katyusha/asgi.py
+++ katyusha/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for kayusha project.
+ASGI config for katyusha project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kayusha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katyusha.settings')
 
 application = get_asgi_application()
