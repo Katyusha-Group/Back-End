40cdf74e5363e3c830dd85d56567e55f3b8ef0e8
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-04-03 12:35:50 +0330

    Add support for postgresql 15.2

--- katyusha/settings.py
+++ katyusha/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'katyusha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'katyusha',
+        'USER': 'postgres',
+        'PASSWORD': '@dmin2023',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
