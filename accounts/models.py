cbd16768a57feaf88b60c6a9ce0d779080e7e7f9
nankaliali <ali.dada.28880@gmail.com>
2023-04-13 02:50:59 +0330

    edit models for others major

--- accounts/models.py
+++ accounts/models.py
@@ -9,15 +9,20 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
    
     is_email_verified = models.BooleanField(default=False)
-    COMPUTER_ENGINEERING = 'CE'
-    ELECTRICAL_ENGINEERING = 'EE'
-    MECHANICAL_ENGINEERING = 'ME'
-    CIVIL_ENGINEERING = 'CE'
-    INDUSTRIAL_ENGINEERING = 'IE'
-    CHEMICAL_ENGINEERING = 'CHE'
-    MATERIAL_ENGINEERING = 'MATE'
-    RAILWAY_ENGINEERING = 'RE'
-    COMPUTER_SCIENCE = 'CS'
+    COMPUTER_ENGINEERING = 'Computer Engineering'
+    ELECTRICAL_ENGINEERING = 'Electrical Engineering'
+    MECHANICAL_ENGINEERING = 'Mechanical Engineering'
+    CIVIL_ENGINEERING = 'Civil Engineering'
+    INDUSTRIAL_ENGINEERING = 'Industrial Engineering'
+    CHEMICAL_ENGINEERING = 'Chemical Engineering'
+    MATERIAL_ENGINEERING = 'Material Engineering'
+    RAILWAY_ENGINEERING = 'Railway Engineering'
+    COMPUTER_SCIENCE = 'Computer Science'
+    MATHEMATICS = 'Mathematics'
+    PHYSICS = 'Physics'
+    CHEMISTRY = 'Chemistry'
+    
+
 
     DEPARTMENT_CHOICES = [
         (COMPUTER_ENGINEERING, 'Computer Engineering'),
@@ -29,8 +34,10 @@ class User(AbstractUser):
         (MATERIAL_ENGINEERING, 'Material Engineering'),
         (RAILWAY_ENGINEERING, 'Railway Engineering'),
         (COMPUTER_SCIENCE, 'Computer Science'),
+        (MATHEMATICS, 'Mathematics'),
+        (PHYSICS, 'Physics'),
     ]
-    department = models.CharField(max_length=4, choices=DEPARTMENT_CHOICES)
+    department = models.CharField(max_length= 50,choices=DEPARTMENT_CHOICES)
 
 
     
