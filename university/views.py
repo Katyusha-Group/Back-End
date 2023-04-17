0058be1eb2b797aaf2af523d5d977e78db82944b
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-04-17 17:11:38 +0330

    Optimize university/views.py and added message to response of my_courses

--- university/views.py
+++ university/views.py
@@ -7,11 +7,9 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from university.pagination import DefaultPagination
-from university.filters import CourseFilter
 from university.models import Course, Department, Semester, ExamTimePlace
 from university.serializers import DepartmentSerializer, SemesterSerializer, SimpleCourseSerializer, \
-    MyCourseSerializer, ModifyMyCourseSerializer, CourseExamTimeSerializer, CourseSerializer
+    ModifyMyCourseSerializer, CourseExamTimeSerializer, CourseSerializer
 from university.scripts import app_variables
 
 
@@ -41,18 +39,14 @@ class CourseViewSet(ModelViewSet):
     http_method_names = ['get', 'put', 'head', 'options']
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
-    filterset_class = CourseFilter
 
     def get_serializer_class(self):
-        course_number = self.request.query_params.get('course_number', None)
         if self.action == 'my_courses' and self.request.method == 'PUT':
             return ModifyMyCourseSerializer
         elif self.action == 'my_courses' and self.request.method == 'GET':
-            return MyCourseSerializer
+            return SimpleCourseSerializer
         elif self.action == 'my_exams' and self.request.method == 'GET':
             return CourseExamTimeSerializer
-        elif course_number is not None and course_number.isdigit():
-            return SimpleCourseSerializer
         return CourseSerializer
 
     def get_queryset(self):
@@ -66,26 +60,31 @@ class CourseViewSet(ModelViewSet):
             if not courses.exists():
                 raise ValidationError(detail='No course with this course_number in database.')
             else:
-                return courses.select_related('base_course')
+                return courses.prefetch_related('teacher', 'course_times', 'exam_times', 'base_course').all()
         except TypeError:
-            return Course.objects.select_related('base_course').all()
+            if self.action != 'my_exams' and self.action != 'my_courses':
+                raise ValidationError(detail='Enter course_number as query string in the url.')
 
     @action(detail=False, methods=['GET', 'PUT'])
     def my_courses(self, request):
         student = get_user_model().objects.get(id=request.user.id)
         if request.method == 'GET':
-            courses = MyCourseSerializer(student.courses.all(), many=True)
+            courses = SimpleCourseSerializer(
+                (student.courses.prefetch_related('teacher', 'course_times', 'exam_times', 'base_course').all()),
+                many=True
+            )
             return Response(status=status.HTTP_200_OK, data=courses.data)
         elif request.method == 'PUT':
             serializer = ModifyMyCourseSerializer(data=request.data, context={'user_id': request.user.id})
             serializer.is_valid(raise_exception=True)
-            serializer.save(student=student)
-            return Response(status=status.HTTP_200_OK)
+            _, created = serializer.save(student=student)
+            message = 'Course added to calendar' if created else 'Course deleted from calendar'
+            return Response(status=status.HTTP_200_OK, data={'message': message})
 
     @action(detail=False, methods=['GET'])
     def my_exams(self, request):
         student = get_user_model().objects.get(id=request.user.id)
-        exam_ids = student.courses.values('exam_times')
+        exam_ids = student.courses.prefetch_related('exam_times', 'base_course').values('exam_times')
         exams = ExamTimePlace.objects.filter(pk__in=exam_ids)
         serializer = CourseExamTimeSerializer(exams, many=True)
         return Response(data=serializer.data)
