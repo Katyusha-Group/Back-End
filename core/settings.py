cf90bb48f78950bbcc8f072caa32875719689343
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-07-03 20:17:28 +0330

    Fix issue in images in sent emails

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'katyushaiust.ir/static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
