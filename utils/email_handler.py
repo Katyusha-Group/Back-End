432198e7e96419dbeef97120ed4763aa74a7bc37
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-07-03 19:45:30 +0330

    Fix issue in sending emails

--- utils/email_handler.py
+++ utils/email_handler.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 from core.settings import EMAIL_HOST
@@ -7,14 +8,22 @@ from core.settings import EMAIL_HOST
 def send_verification_message(subject, recipient_list, verification_token):
     context = {
         'email_verification_token': verification_token,
+        'image_url1': 'http://katyushaiust.ir/static/Back-End/katyusha-activation-image.png'
     }
     html_message = render_to_string('activation_template.html', context)
-    send_mail(subject, "", EMAIL_HOST, recipient_list, html_message=html_message)
+    email = EmailMultiAlternatives(subject, '', EMAIL_HOST, recipient_list)
+    email.attach_alternative(html_message, "text/html")
+    email.send()
 
 
 def send_forget_password_verification_message(subject, recipient_list, verification_token):
     context = {
         'email_verification_token': verification_token,
+        'image_bg': 'http://katyushaiust.ir/static/Back-End/ResetPassword_BG_2.png',
+        'image_icon': 'http://katyushaiust.ir/static/Back-End/kat_1.png',
+        'image_header': 'http://katyushaiust.ir/static/Back-End/Reset_password-bro.png',
     }
     html_message = render_to_string('forget_password.html', context)
-    send_mail(subject, "", EMAIL_HOST, recipient_list, html_message=html_message)
+    email = EmailMultiAlternatives(subject, '', EMAIL_HOST, recipient_list)
+    email.attach_alternative(html_message, "text/html")
+    email.send()
