c45a4ec0485be20eec77ee73989e35d89170466b
Ali Nankali <101474176+nankaliali@users.noreply.github.com>
2023-07-08 15:29:55 +0330

    fix issue telegram_notification.py

--- botapp/telegram_notification.py
+++ botapp/telegram_notification.py
@@ -17,18 +17,15 @@ def send_telegram_notification(email=None, changes=None):
 
     token = '6182994088:AAFZbZ9_fMeWebvb4x9_vb3k4q74RYWAuOM'
 
-    # if user is None:
-    #     raise ValueError('user_id is required')
-    # if changes is None:
-    #     raise ValueError('changes is required')
-
+   
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_telegram_message(token, response['user_id'], changes))
-    loop.close()
 
 
 if __name__ == '__main__':
-    send_telegram_notification(email='ali.nankali.288@gmail.com', changes='خوبی؟')
+    for i in range(10):
+        send_telegram_notification(email='ali.nankali.288@gmail.com', changes=f"{i}th message")
+
 #
 
 
