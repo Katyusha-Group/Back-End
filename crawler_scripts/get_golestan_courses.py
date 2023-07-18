53350d365a6757ade3f20cd2ea00875ea8890c55
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-07-18 12:16:15 +0330

    Cleaning Crawlers

--- crawler_scripts/get_golestan_courses.py
+++ crawler_scripts/get_golestan_courses.py
@@ -3,7 +3,7 @@ from crawler_scripts.golestan_crawler import GolestanCrawler
 
 def get_golestan_courses(year):
     crawler = GolestanCrawler(
-        user_login=False, username="99522104", password="0926190466", year=year
+        user_login=False, year=year
     )
     crawler.login()
     crawler.get_courses()
