cada240e91932303ac973ad670787c3cd38b414d
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-10-18 23:39:20 +0330

    Added documents for university/urls

--- university/urls.py
+++ university/urls.py
@@ -9,13 +9,20 @@ router.register(r'teachers', viewset=views.TeacherViewSet, basename='teachers')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('semeters/', views.SemesterViewList.as_view(), name='semesters'),
+    # semesters:
+    path('semeters/', views.SemestersViewList.as_view(), name='semesters'),
+
+    # departments:
     path('departments/', views.DepartmentsListView.as_view(), name='departments'),
     path('departments/names', views.SignupDepartmentListView.as_view(), name='department_names'),
     path('departments/sorted-names', views.SortedNamesDepartmentListView.as_view(), name='sorted_department_names'),
+
+    # timelines:
     path('timeline/courses/<int:course_number>', views.BaseCoursesTimeLineListAPIView.as_view(),
          name='course_timeline'),
     path('timeline/teachers/<int:teacher_id>', views.TeachersTimeLineListAPIView.as_view(), name='teacher_timeline'),
+
+    # courses:
     path('coursegroups/<base_course_id>', views.CourseGroupListView.as_view({'get': 'list'}), name='course-groups'),
     path('allcoursesdepartment/', views.AllCourseDepartmentList.as_view(), name='all-course-department'),
     path('allcourses-based-department/<int:department_id>', views.AllCourseDepartmentRetrieve.as_view(),
