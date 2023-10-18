167f748edf49a8492265acc0d82646c18931638d
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-10-18 22:50:33 +0330

    modified signals, and provided comments for them.

--- custom_config/signals/__init__.py
+++ custom_config/signals/__init__.py
@@ -1,3 +1,6 @@
 from django.dispatch import Signal
 
-order_created = Signal(providing_args=['order', 'total_price'])
+order_created = Signal()
+# This signal provides following arguments:
+# - order: The order that is created
+# - total_price: The total price of the order
