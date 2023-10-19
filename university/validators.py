b8cdd65f39f3b973cf73f033bddf15e7fe8d7057
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-10-19 23:13:55 +0330

    Fixed bug in university/validators.py

--- university/validators.py
+++ university/validators.py
@@ -6,10 +6,10 @@ from rest_framework.exceptions import ValidationError
 def validate_int(expected_int, message):
     try:
         validators.validate_integer(expected_int)
-    except ValidationError:
+    except:
         raise ValidationError({'detail': message})
 
 
 def validate_queryset_existence(queryset: QuerySet, message: str):
     if not queryset.exists():
-        raise ValidationError({'detail': 'course does not exist'})
+        raise ValidationError({'detail': message})
