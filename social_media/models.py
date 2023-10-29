80a9205e73f3425047488b1b205ae2103423b404
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-10-29 13:49:45 +0330

    fix(Profile): fixed bug in Profile model

--- social_media/models.py
+++ social_media/models.py
@@ -5,8 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-from university.models import Course, Teacher
-from utils.variables import project_variables
+from university.models import BaseCourse, Teacher
 
 
 class Profile(models.Model):
@@ -22,20 +21,27 @@ class Profile(models.Model):
         (TYPE_VERIFIED_USER, 'کاربر تاییدشده')
     )
 
-    content_type = models.OneToOneField(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+
     name = models.CharField(max_length=100, blank=True, null=True)
     username = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='images/profile_pics', default='images/profile_pics/default.png')
     profile_type = models.CharField(max_length=1, choices=ACTION_CHOICES, default=TYPE_USER)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+        unique_together = ('content_type', 'object_id')
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
         user_model = get_user_model()
-        if not isinstance(self.content_object, (Course, Teacher, user_model)):
+        if not isinstance(self.content_object, (BaseCourse, Teacher, user_model)):
             raise ValidationError('Invalid value for content_object')
         self.name = self.determine_name(self.name)
         self.username = self.determine_username(self.username)
@@ -65,7 +71,7 @@ class Profile(models.Model):
                 return self.TYPE_USER
             if isinstance(self.content_object, Teacher):
                 return self.TYPE_TEACHER
-            if isinstance(self.content_object, Course):
+            if isinstance(self.content_object, BaseCourse):
                 return self.TYPE_COURSE
             # TODO: determine profile type for verified users
             return None
