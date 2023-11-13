b472f5b07a5db8fb56c4185d606549fa7b12101e
mhhasani <hasanimohammadhosein@gmail.com>
2023-11-14 00:13:45 +0330

    change db to sqlite3

--- core/settings.py
+++ core/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'katyush2_db',
-        'USER': 'katyush2_admin',
-        'PASSWORD': 'katyusha@dmin2023',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
