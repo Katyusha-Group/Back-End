b2d776aab7897a2704aedc8b7243550964401c47
Ali Nankali <101474176+nankaliali@users.noreply.github.com>
2023-11-24 22:35:20 +0330

    fix: Resolve Celery and Redis bugs in Docker Compose
    - Updated Docker Compose configuration to fix Celery worker connection issues.
    - Resolved Redis configuration problems causing bugs in the setup.

--- core/celery.py
+++ core/celery.py
@@ -5,8 +5,16 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('core')
+app.conf.enable_utc = False
+
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    #print("setup_periodic_tasks")
+    sender.add_periodic_task(10.0, print.s('Hello World'), name='add every 10')
