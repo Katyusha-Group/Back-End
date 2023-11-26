b7521bbb970f898bc89e7717806efb3e2ab4a515
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-11-26 23:34:56 +0330

    refactor(notification): made changes to NotificationQuerySet

--- social_media/querysets.py
+++ social_media/querysets.py
@@ -2,20 +2,15 @@ from django.db import models
 
 
 class NotificationQuerySet(models.QuerySet):
-    def unread_notifications(self, recipient=None):
+    def unread(self, recipient=None):
         if recipient:
             return self.filter(recipient=recipient, read=False)
         return self.filter(read=False)
 
-    def read_notifications(self, recipient=None):
+    def read(self, recipient=None):
         if recipient:
             return self.filter(recipient=recipient, read=True)
         return self.filter(read=True)
-
-    def all_notifications(self, recipient=None):
-        if recipient:
-            return self.filter(recipient=recipient)
-        return self.all()
 
     def mark_all_as_read(self, recipient=None):
         if recipient:
