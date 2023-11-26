42289e12ca6db5f99a3920eac794d5a64efbb6c5
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-11-26 21:42:29 +0330

    notification: add signal for handling creation of new notifications

--- social_media/signals/handlers.py
+++ social_media/signals/handlers.py
@@ -3,9 +3,10 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from social_media.models import Profile
+from social_media.models import Profile, Notification
 from university.models import Teacher, BaseCourse
 from accounts.models import User
+from social_media.signals import send_notification
 
 
 @receiver(post_save, sender=User)
@@ -15,3 +16,25 @@ def create_profile(sender, **kwargs):
     if kwargs['created']:
         content_type = ContentType.objects.get_for_model(sender)
         Profile.objects.create(content_type=content_type, object_id=kwargs['instance'].pk)
+
+
+@receiver(send_notification)
+def send_notification_handler(sender, **kwargs):
+    recipient = kwargs['recipient']
+    notification_type = kwargs['notification_type']
+    actor = kwargs['actor']
+
+    if notification_type == Notification.TYPE_FOLLOW:
+        Notification.objects.create(
+            recipient=recipient,
+            actor=actor,
+            notification_type=notification_type,
+        )
+    else:
+        tweet = kwargs['tweet']
+        Notification.objects.create(
+            recipient=recipient,
+            actor=actor,
+            notification_type=notification_type,
+            tweet=tweet
+        )
