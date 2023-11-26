fa5701241a94ea33ca6762147776de572daa509a
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-11-26 23:36:53 +0330

    notification: add notification apis

--- core/urls.py
+++ core/urls.py
@@ -36,9 +36,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('', include('social_media.urls')),
     path('', include('custom_config.urls')),
     path('', include('university.urls')),
-    path('', include('social_media.urls')),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- social_media/urls.py
+++ social_media/urls.py
@@ -6,6 +6,7 @@ from rest_framework_nested import routers
 router = routers.DefaultRouter()
 router.register(r'profiles', views.ProfileViewSet, basename='profiles')
 router.register(r'twittes', views.TwitteViewSet, basename='twittes')
+router.register(r'notifications', views.NotificationViewSet, basename='notifications')
 urlpatterns = [
     path('', include(router.urls)),
 ]
