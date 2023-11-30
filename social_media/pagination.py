eb801fb6f16967ac09f4577e632613aa79705c5e
mhhasani <hasanimohammadhosein@gmail.com>
2023-11-30 13:18:06 +0330

    fix: change page_size

--- social_media/pagination.py
+++ social_media/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DefaultPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
