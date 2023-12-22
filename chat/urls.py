b7575ac6117e118b94d8a164455911af97bd1233
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-12-23 01:01:48 +0330

    chat: configured chat urls

---
+++ chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+from rest_framework_nested import routers
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+]
--- core/urls.py
+++ core/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('bot/', include('botapp.urls')),
+    path('chat/', include('chat.urls')),
     # path('__debug__/', include('debug_toolbar.urls')),
 ]
 
