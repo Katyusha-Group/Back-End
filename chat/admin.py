73648a1fe84c35c3c87ec7a37b79423850778f57
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-12-23 01:10:29 +0330

    chat: add new models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Contact, Message, Chat
+
+admin.site.register(Contact)
+admin.site.register(Message)
+admin.site.register(Chat)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from core.settings import AUTH_USER_MODEL
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(
+        AUTH_USER_MODEL, related_name='friends', on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Message(models.Model):
+    author = models.ForeignKey(
+        Contact, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.user.username
+
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(
+        Contact, related_name='chats', blank=True)
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.pk)
