a50636c220146465bb7a48ce7b9edd282b283c64
Kamyar Moradian <kamyarmorad1381@gmail.com>
2023-12-29 16:01:37 +0330

    fix: fixed issue in asgi

--- core/asgi.py
+++ core/asgi.py
@@ -16,7 +16,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.setting.dev')
 
 django.setup()
 
